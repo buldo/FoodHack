ea01fffc3b1c80dbc9433b14de17acf390c40c2b
Булдыгин Роман Андреевич <roman.buldygin@skbkontur.ru>
2018-03-04 02:18:18 +0300

    Добавил несколько VM и клиент к сервису

---
+++ back/MyFood/MyFood.EditorApp/ViewModels/PreviewViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace MyFood.EditorApp.ViewModels
+{
+    internal class PreviewViewModel : BindableBase
+    {
+        public PreviewViewModel(string title, string previewUrl)
+        {
+            Title = title;
+            PreviewUrl = previewUrl;
+        }
+
+        public string Title { get; }
+
+        public string PreviewUrl { get; }
+    }
+}
